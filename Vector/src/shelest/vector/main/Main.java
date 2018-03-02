68d88880031c4bb51ae1728e751ff1b460830c72
Станислав <shelest_stas@mail.ru>
2018-03-03 02:46:45 +0600

    первая проверка

--- Vector/src/shelest/vector/main/Main.java
+++ Vector/src/shelest/vector/main/Main.java
@@ -2,23 +2,45 @@ package shelest.vector.main;
 
 import shelest.vector.operations.Vector;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double[] proba3 = {12, 23, 12, 11, 45, 67, 765};
-        double[] proba4 = {5, 7, 8, 5, 12};
-        Vector vector3 = new Vector(proba3);
-        Vector vector4 = new Vector(proba4);
+        double[] componentsArray1 = {5, -7, 8.3, 5, 12};
+        double[] componentsArray2 = {23, 45, 78, 12, -14, 98.8};
+        Vector vector1 = new Vector(8);
+        Vector vector2 = new Vector(componentsArray1);
+        Vector vector3 = new Vector(vector2);
+        Vector vector4 = new Vector(vector2);
+        Vector vector5 = new Vector(8, componentsArray2);
 
-        double scalar = 2;
+        System.out.print("Введите скаляр: ");
+        Scanner scanner1 = new Scanner(System.in);
+        double scalar = scanner1.nextDouble();
 
-        System.out.println(vector3.toString());
-        //System.out.println(vector4.toString());
+        System.out.print("Введите индекс компонента: ");
+        Scanner scanner2 = new Scanner(System.in);
+        int index = scanner2.nextInt();
 
-        //System.out.println(vector3.getAddition(vector4).toString());
-        //System.out.println(vector3.getSubtraction(vector4).toString());
-        //System.out.println(vector3.getMultiplicationByScalar(scalar).toString());
-        //System.out.println(vector3.getTurn().toString());
-        //System.out.println(vector3.getLength());
-        System.out.println(vector3.getInstallationComponent(2,654));
+        System.out.print("Введите компонент: ");
+        Scanner scanner3 = new Scanner(System.in);
+        double addedComponent = scanner3.nextDouble();
+
+        System.out.println("1. " + vector1.toString());
+        System.out.println("2. " + vector2.toString());
+        System.out.println("3. " + vector3.toString());
+        System.out.println("4. " + vector3.toString());
+        System.out.println("5. " + vector5.toString());
+
+        System.out.println(System.lineSeparator() + "Длина третьего вектора составляет: " + vector3.getSize());
+        System.out.println("Результат сложения второго и пятого векторов: " + vector2.getAddition(vector5).toString());
+        System.out.println("Результат разности второго и пятого векторов: " + vector2.getSubtraction(vector5).toString());
+        System.out.println("Результат умножения второго вектора на 2: " + vector2.getMultiplicationByScalar(scalar).toString());
+        System.out.println("Результат разворота второго вектора: " + vector2.getTurn().toString());
+        System.out.println("Длина второго вектора: " + vector2.getLength());
+        System.out.println("Результат замены компонента пятого вектора: " + vector5.getInstallationComponent(index, addedComponent));
+        System.out.println("Результат равенства второго и четвертого вектора: " + vector2.equals(vector4));
+        System.out.println("hashCode второго вектора: " + vector2.hashCode());
+        System.out.println("hashCode четвертого вектора: " + vector4.hashCode());
     }
 }
--- Vector/src/shelest/vector/operations/Vector.java
+++ Vector/src/shelest/vector/operations/Vector.java
@@ -6,38 +6,46 @@ public class Vector {
     private int dimension;
     private double[] components;
 
-    public Vector (int dimension){
-        this.dimension = dimension;
-        this.components = new double [dimension];
+    public int getDimension() {
+        return dimension;
     }
 
-    public Vector (Vector vector){
+    public double[] getComponents() {
+        return components;
+    }
+
+    public Vector(int dimension) {
+        this.dimension = dimension;
+        this.components = new double[dimension];
+    }
+
+    public Vector(Vector vector) {
         this.dimension = vector.dimension;
         this.components = Arrays.copyOf(vector.components, vector.components.length);
     }
 
-    public Vector (double[] components){
+    public Vector(double[] components) {
         this.dimension = components.length;
         this.components = Arrays.copyOf(components, components.length);
     }
 
-    public Vector (int dimension, double[] components){
+    public Vector(int dimension, double[] components) {
         this.dimension = dimension;
         this.components = Arrays.copyOf(components, dimension);
     }
 
-    public int getSize (){
+    public int getSize() {
         return this.dimension;
     }
 
-    public String toString (){
-       return Arrays.toString(this.components);
+    public String toString() {
+        return Arrays.toString(this.components);
     }
 
     public Vector getAddition(Vector vector) {
         int minDimension;
         double[] resultAddition;
-        if (this.dimension >= vector.dimension){
+        if (this.dimension >= vector.dimension) {
             minDimension = vector.dimension;
             resultAddition = Arrays.copyOf(this.components, this.components.length);
         } else {
@@ -50,11 +58,11 @@ public class Vector {
         return new Vector(resultAddition);
     }
 
-    public Vector getSubtraction (Vector vector){
+    public Vector getSubtraction(Vector vector) {
         int count;
         double[] minuend;
         double[] subtrahend;
-        if (this.dimension >= vector.dimension){
+        if (this.dimension >= vector.dimension) {
             count = this.dimension;
             minuend = Arrays.copyOf(this.components, this.dimension);
             subtrahend = Arrays.copyOf(vector.components, this.dimension);
@@ -70,32 +78,54 @@ public class Vector {
         return new Vector(resultSubtraction);
     }
 
-    public Vector getMultiplicationByScalar (double scalar){
-        double [] resultMultiplication = new double[this.dimension];
-        for (int i = 0; i < this.dimension; i++){
-            resultMultiplication [i] = this.components[i] * scalar;
+    public Vector getMultiplicationByScalar(double scalar) {
+        double[] resultMultiplication = new double[this.dimension];
+        for (int i = 0; i < this.dimension; i++) {
+            resultMultiplication[i] = this.components[i] * scalar;
         }
         return new Vector(resultMultiplication);
     }
 
-    public Vector getTurn (){
-        double [] resultTurn = new double[this.dimension];
-        for (int i = 0; i < this.dimension; i++){
-            resultTurn [i] = this.components[i] * (-1);
+    public Vector getTurn() {
+        double[] resultTurn = new double[this.dimension];
+        for (int i = 0; i < this.dimension; i++) {
+            resultTurn[i] = this.components[i] * (-1);
         }
         return new Vector(resultTurn);
     }
 
-    public double getLength (){
-        double sum = Math.pow(this.components[0],2);
-        for (int i = 1; i < this.dimension; i++){
-            sum = sum + Math.pow(this.components[i],2);
+    public double getLength() {
+        double sum = Math.pow(this.components[0], 2);
+        for (int i = 1; i < this.dimension; i++) {
+            sum = sum + Math.pow(this.components[i], 2);
         }
         return Math.sqrt(sum);
     }
 
-    public Vector getInstallationComponent (int index, double installableComponent){
+    public Vector getInstallationComponent(int index, double installableComponent) {
         this.components[index] = installableComponent;
         return new Vector(this.components);
+    }
+
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        Vector vector = (Vector) o;
+        return (this.getDimension() == vector.getDimension() || Arrays.equals(this.getComponents(), vector.getComponents()));
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (Arrays.equals(getComponents(), null) ? 0 : Arrays.hashCode(getComponents()));
+        result = prime * result + getDimension();
+        return result;
     }
 }
